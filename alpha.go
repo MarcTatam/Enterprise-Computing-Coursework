5d3b83e46b85b24841041fe818431ce531a67a6b
MarcTatam <marc.smsjb.tatam@gmail.com>
2022-02-16 12:00:14 +0000

    Complete error handling

--- alpha.go
+++ alpha.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"github.com/gorilla/mux"
 	"io"
@@ -20,28 +21,57 @@ type Keys struct {
 
 var keys Keys
 
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
 func AlphaReq(question string) (string, error) {
 	client := &http.Client{}
 	uri := URI + "?appid=" + keys.Alpha + "&i=" + url.QueryEscape(question)
 	req, err := http.NewRequest("GET", uri, nil)
+	if err != nil {
+		return "", err
+	}
 	rsp, err := client.Do(req)
-	fmt.Println(rsp.StatusCode)
+	if err != nil {
+		return "", err
+	}
+	if rsp.StatusCode != 200 {
+		return "", errors.New("wolfram alpha didn't like that")
+	}
 	b, err := io.ReadAll(rsp.Body)
+	if err != nil {
+		return "", err
+	}
 	return string(b), nil
 }
 
 func Alpha(w http.ResponseWriter, r *http.Request) {
 	t := map[string]interface{}{}
 	if err := json.NewDecoder(r.Body).Decode(&t); err == nil {
-		question := t["text"].(string)
-		answer, err := AlphaReq(question)
-		if err == nil {
-			responseBody := map[string]interface{}{"text": answer}
-			w.WriteHeader(http.StatusOK)
-			json.NewEncoder(w).Encode(responseBody)
+		question, present := t["text"].(string)
+		if present {
+			answer, err := AlphaReq(question)
+			if err == nil {
+				responseBody := map[string]interface{}{"text": answer}
+				w.WriteHeader(http.StatusOK)
+				err := json.NewEncoder(w).Encode(responseBody)
+				if err != nil {
+					w.WriteHeader(http.StatusInternalServerError)
+					_, err := w.Write([]byte("Could not encode response"))
+					check(err)
+				}
+			} else {
+				w.WriteHeader(http.StatusInternalServerError)
+				_, err := w.Write([]byte("Something went wrong when contacting the Alpha API"))
+				check(err)
+			}
 		} else {
-			w.WriteHeader(http.StatusInternalServerError)
-			w.Write([]byte("Something went wrong when contacting the Alpha API"))
+			w.WriteHeader(http.StatusBadRequest)
+			_, err = w.Write([]byte("Incorrectly Formatted JSON"))
+			check(err)
 		}
 	} else {
 		w.WriteHeader(http.StatusBadRequest)
@@ -63,6 +93,6 @@ func main() {
 	r := mux.NewRouter()
 	// document
 	r.HandleFunc("/alpha", Alpha).Methods("POST")
-	err = http.ListenAndServe(":3000", r)
+	err = http.ListenAndServe(":3001", r)
 	check(err)
 }
